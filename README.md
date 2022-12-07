322c41ed3b542d7a887e303469b62d79a3311ac0
saiani1 <saiani@naver.com>
2022-12-07 22:16:13 +0900

    Fix: 빌드 오류 코드 수정

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # wanted-pre-onboarding-frontend
 
-# 프로젝트의 실행 방법
+## 1. 프로젝트 실행방법
 ```
 $ npm install
 $ npm run start
 ```
 
-# 배포링크
+## 2. 배포링크
 
-# 구현기능
+
+## 3. 구현기능
 모든 요구사항 구현 완료
--- src/routes/Todos/index.jsx
+++ src/routes/Todos/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-console */
 import { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
